713d294d601d3a726de2b9be7e99f6b3b3ee470b
arianais <ariananis@aol.com>
2015-07-24 20:06:42 -0700

    added web home

---
+++ AZBrainFoodApp/AZBrainFoodApp/WebHomeViewController.h
@@ -0,0 +1,14 @@
+//
+//  WebHomeViewController.h
+//  AZBrainFoodApp
+//
+//  Created by Ariana on 7/24/15.
+//  Copyright (c) 2015 Logical Nonsense Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebHomeViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *back;
+
+@end
